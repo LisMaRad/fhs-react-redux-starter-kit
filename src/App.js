16de014587a7897cbfa4687c214c5e071084a256
fhs45918 <fhs45918@fh-salzburg.ac.at>
2022-03-19 11:14:01 +0100

    add routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import { React, useState } from 'react'
-import { SignIn } from './components/Form'
+import { SignIn, SignUp } from './components/Form'
+import { MoneyTransaction } from './components/MoneyTransaction'
+import { BrowserRouter as Router, Route, Switch, Redirect, Routes } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
-function App () {
+function App() {
   return (
-    <div>
-        <SignIn />
-    </div>
-  )
-}
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/sign-in">SignIn</Link>
+            </li>
+            <li>
+              <Link to="/sign-up">SignUp</Link>
+            </li>
+            <li>
+              <Link to="/money-transaction">Money Transaction</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+        <Route path="/" element={<h1>Startpage</h1>}>
+          </Route>
+          <Route exact path="/sign-in" element={<SignIn />}>
+          </Route>
+          <Route path="/sign-up" element={<SignUp />}>
+          </Route>
+          <Route path="/money-transaction" element={<MoneyTransaction />}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+} 
 
 export default App
